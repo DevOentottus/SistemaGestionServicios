85b522a2b6bb3f818d39402f39324e9d10ecb6a4
DevOentottus <jhan490000@gmail.com>
2026-04-27 13:00:10 -0500

    none

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,8 @@ import { supabase } from '../lib/supabase';
 
 export default function App() {
   // ✅ El useEffect debe ir DENTRO del componente
+  console.log('URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('ANON:', import.meta.env.VITE_SUPABASE_ANON_KEY);
   useEffect(() => {
     const testConnection = async () => {
       const { data, error } = await supabase
