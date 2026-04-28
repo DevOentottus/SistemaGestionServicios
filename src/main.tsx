fba4f83e7dc2e0a172769f76e73d31b113d4930b
DevOentottus <jhan490000@gmail.com>
2026-04-28 12:13:35 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/routes';
 import { AuthProvider } from './context/AuthContext';
+import App from './app/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
+    <App />
   </React.StrictMode>
+  
 );
