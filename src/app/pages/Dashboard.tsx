4d137513cb301026f8cec16aab7abe3d74fbd844
DevOentottus <jhan490000@gmail.com>
2026-04-28 10:48:53 -0500

    RR

--- src/app/pages/Dashboard.tsx
+++ src/app/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { servicios, colaboradores, areas, solicitudes, auditLogs } from "../data/mockData";
 import {
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 
   import { createRoot } from "react-dom/client";
+  import { RouterProvider } from "react-router-dom";
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
