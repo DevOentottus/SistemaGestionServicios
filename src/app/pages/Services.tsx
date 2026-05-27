45b76dcf25ad984f17744f819f05058fdb96c9b5
DevOentottus <jhan490000@gmail.com>
2026-05-27 12:33:28 -0500

    Update Services.tsx

--- src/app/pages/Services.tsx
+++ src/app/pages/Services.tsx
@@ -146,7 +146,7 @@ const formatElapsedShort = (sec: number): string => {
   const h = Math.floor((sec % 86400) / 3600);
   const m = Math.floor((sec % 3600) / 60);
   const s = sec % 60;
-  if (d > 0) return `${d}d ${h}h`;
+  if (d > 0) return `${d}d ${h}h ${m}m`;
   if (h > 0) return `${h}h ${m}m`;
   if (m > 0) return `${m}m ${s}s`;
   return `${s}s`;
